7bd1511c5fd54c16f6f57fe9a0f2688a6315470f
David-Miguel007 <146042071+David-Miguel007@users.noreply.github.com>
2023-10-01 11:59:31 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,23 @@
-const botoes =document.querySelectorAll(".botao")
-const personagens =document.querySelectorAll(".personagem")
+const botoes = document.querySelectorAll(".botao")
+const personagens = document.querySelectorAll(".personagem")
+
 
 botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () => {
-        const botaoSelecionado =document.querySelector(".botao.selecionado")
-        const personagemSelecionado =document.querySelector(".personagem.selecionado")
+        desselecionarBotao()
+        desselecionarPersonagem()
+
         botao.classList.add("selecionado")
-        botaoSelecionado.classList.remove("selecionado")
         personagens[indice].classList.add("selecionado")
-        personagemSelecionado.classList.remove("selecionado")
     })
 })
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado")
+    personagemSelecionado.classList.remove("selecionado")
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado")
+    botaoSelecionado.classList.remove("selecionado")
+}
